93076667bae276d2ce6bc65db5d350f34034056e
NTSAKO-coder <janelntsako@gmail.com>
2025-01-16 16:27:52 +0200

    code updated

--- NotFacebook-main/JS/Splash.js
+++
@@ -1,5 +0,0 @@
-window.onload = () => {
-    setTimeout(() => {
-        window.location.href = 'http://127.0.0.1:5500/NotFacebook-main/HTML/Login.html'; 
-    }, 3000); 
-};
---
+++ NotFacebook-main/JS/UserProfile.js
@@ -0,0 +1,8 @@
+function showProfile() {
+    const profileSection = document.getElementById('profile-section');
+    profileSection.style.display = 'block';
+}
+function hideProfile() {
+    const profileSection = document.getElementById('profile-section');
+    profileSection.style.display = 'none';
+}
